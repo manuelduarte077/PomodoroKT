05474a15fb6a58d181357fa78480e5d27b5b5fe1
manuelduarte077 <carlosd99m@gmail.com>
2025-06-24 02:26:02 -0600

    wip: initial code

---
+++ composeApp/src/desktopTest/kotlin/dev/donmanuel/app/pomodoro/ComposeAppDesktopTest.kt
@@ -0,0 +1,12 @@
+package dev.donmanuel.app.pomodoro
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppDesktopTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
